f7f538cd32f2cc125665afdb277438b0881bd878
EliseoSantos2468 <sd20007@ues.edu.sv>
2025-12-23 15:51:05 -0600

    Añadir sidebar y mejorar estilos de navegación con variables CSS

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -6,10 +6,16 @@ use Illuminate\View\Component;
 use Illuminate\View\View;
 
 class AppLayout extends Component
-{
-    /**
-     * Get the view / contents that represents the component.
-     */
+{   
+    // colores
+    public 
+            $primaryColor ="rgb(71 85 105 / var(--tw-bg-opacity, 1))",
+            $secondaryColor ="#3b82f6",
+            $white= '#ffffff',
+            $black= '#000000'
+            ;
+
+
     public function render(): View
     {
         return view('layouts.app');
